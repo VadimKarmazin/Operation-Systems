6afa7976155df774ee7fd9f7400d68b325a8a078
Vadim <99864658+VadimKarmazin@users.noreply.github.com>
2022-12-12 00:12:25 +0300

    Create main.py

---
+++ Практика 4/Python/main.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import time
+import ctypes
+a = ctypes.c_uint(0)
+b = 2
+c = 3
+i = 0
+start_time = datetime.now()
+
+while i < 100000000:
+    a.value += b * 2 + c - i
+    i += 1
+print(a)
+print(datetime.now() - start_time)
